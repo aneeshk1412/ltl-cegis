df006dcaaa21cf790f231d255bfc423837449962
Aneesh Shetty <aneeshkshetty@gmail.com>
2022-10-05 10:01:04 -0500

    Adds type interpretation enums and non_deterministic function

--- parse_description.py
+++ parse_description.py
@@ -11,26 +11,40 @@ with open("descriptions/structure.json", "rb") as f:
 
 cw = CodeWriter()
 
+cw.add_line("#include <stdio.h>")
+cw.add_line("")
+cw.add_lines([
+    "extern void __VERIFIER_error() __attribute__ ((__noreturn__));", 
+    "extern void __VERIFIER_assume() __attribute__ ((__noreturn__));",
+    "extern int __VERIFIER_nondet_int() __attribute__ ((__noreturn__));",
+])
+cw.add_line("")
+
 ''' Parse position properties '''
 for p in config["position properties"]:
-    enum = Enum(f"IS_{p['name']}", prefix=p['name'], typedef=True)
-    enum.add_value("_NO", 0)
-    enum.add_value("_YES", 1)
-    cw.add_enum(enum)
-    cw.add_line("")
-
-    b = 1 if p['type'] else 0
+    b = 1 if p['include'] else 0
 
     func = Function(f"check_{p['name']}", "int")
     for dim in range(config['dimensions']):
         func.add_argument((f"x_{dim}", "int"))
     for rnge in p['ranges']:
-        cond = ""
+        cond = []
         for dim in range(config['dimensions']):
-            cond += f"x_{dim} >= {rnge[dim][0]} && x_{dim} <= {rnge[dim][1]}"
-        func.add_code(f"if ({cond}) return {b};")
+            if rnge[dim][0] == rnge[dim][1]:
+                cond.append(f"x_{dim} == {rnge[dim][0]}")
+            else:
+                cond.append(f"x_{dim} >= {rnge[dim][0]} && x_{dim} <= {rnge[dim][1]}")
+        func.add_code(f"if ({' && '.join(cond)}) return {b};")
     func.add_code(f"return {1-b};")
     cw.add_function_definition(func)
     cw.add_line("")
+
+for t in config["type interpretations"]:
+    if t["type"] == "enum":
+        enum = Enum(f"{t['name']}", prefix=t['name']+"_", typedef=True)
+        for val in t["range"]:
+            enum.add_value(val["name"])
+        cw.add_enum(enum)
+        cw.add_line("")
 
 print(cw)
