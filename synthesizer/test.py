2ff715e007c58799ad26ede1051c0cedf5fe780f
Kiarash Rahmani Admin <kiar-admin@utcsadmins-Air.lan>
2022-10-09 21:55:16 -0500

    new DSL

--- synthesizer/test.py
+++ synthesizer/test.py
@@ -3,16 +3,24 @@ __author__ = "Kia Rahmani"
 
 
 """A simple test program which repeatedly creates and prints programs using a synthesizer """
+from os import defpath
 import sys
+import random
 from src.synthesizer import *
+
 
 def main(arguments):
     synth = Synthesizer()
-    asps = synth.enumerate_all_asps()
-    print (len(asps), ' ASPs of length=1 are generated')
-    # pick an ASP and print it
-    print(asps[10].pretty_str())
+    policy_depth = 2
+    asps = synth.enumerate_all_asps(depth=policy_depth, cap=4000)
+    number_of_asps = len(asps)
+    for asp in asps:
+        print(asp.pretty_str())
+        print (50*'-')
+    print (number_of_asps, ' ASPs of length =', policy_depth,'are generated')
 
+        
+    
 
 
 if __name__ == '__main__':
