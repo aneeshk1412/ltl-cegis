c5034dda4a1f45a0af6f734ae499ab79ac7f49ce
Aneesh Shetty <aneeshkshetty@gmail.com>
2022-11-10 12:39:29 -0600

    completes model program generator

--- verifier/modelprog.py
+++ verifier/modelprog.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
+from itertools import count
 from csnake import (
     CodeWriter, Function, Variable
 )
 
+global_counter = count(100)
+
 def define_liveness_spec(config):
+    ''' <TODO> Parse the liveness spec from DSL kind language '''
     cw = CodeWriter()
-    cw.add_line(f'//@ ltl invariant positive: {config["LivenessSpec"]};')
+    cw.add_line(f'//@ ltl invariant positive: {config["livenessSpec"]};')
     return cw
 
 def define_imports_and_extern():
@@ -22,31 +26,43 @@ def define_imports_and_extern():
     return cw
 
 def variables_action_enums(config):
-    return {action: Variable(action, 'int', value=details['value']) for action, details in config['Action'].items()}
+    d = dict()
+    for _, actions_dict in config['actionTypes'].items():
+        d.update({action: Variable(action, 'int', value=c) for action, c in zip(actions_dict['actions'], global_counter)})
+    return d
 
 def variables_state(config):
     mapping = dict()
-    for statevar, details in config['State'].items():
+    for statevar, details in config['state'].items():
         if details['type'] == 'Vector':
             vector_map = dict()
-            for dim in config['Dimensions']:
+            for dim in config['dimensions']:
                 vector_map[dim] = Variable(statevar + dim, 'int')
             mapping[statevar] = vector_map
+        elif details['type'] in config['actionTypes']:
+            mapping[statevar] = Variable(statevar, 'int')
         else:
             mapping[statevar] = Variable(statevar, details['type'])
     return mapping
 
-def function_action_semantics_for_var(config, var):
-    func = Function(f'update_{var}_from_action', 'void', arguments=[('act', 'int')])
-    for action, details in config['Action'].items():
-        # <TODO> parse act['update'] from dsl form to C form
-        func.add_code(f"if (act == {action}) {details['update'].replace('p', var)};")
+def function_action_semantics_for_act_var(config, act_var):
+    var_mapping = config['state'][act_var]['mapping']
+    var_type = config['state'][act_var]['type']
+    func = Function(f'apply_{act_var}', 'void')
+    lines = ''
+    for action, details in config['actionTypes'][var_type]['actions'].items():
+        # <TODO> parse details['update'] from dsl form to C form
+        lines += f"if ({act_var} == {action}) {details['update']};\n"
+    for k, v in var_mapping.items():
+        lines = lines.replace(k + '.', v)
+    func.add_code(lines)
+    print(func.generate_definition())
     return func
 
 def function_static_property_function(prop, details, config):
     ''' Returns check_prop_X(Position) function for static property X '''
     bit = 1 if details['include'] else 0
-    func = Function(f"check_prop_{prop}", 'int', arguments=[(f'p{dim}', 'int') for dim in config['Dimensions']])
+    func = Function(f"check_prop_{prop}", 'int', arguments=[(f'p{dim}', 'int') for dim in config['dimensions']])
     for box in details['ranges']:
         cond = []
         for k, rnge in box.items():
@@ -61,14 +77,14 @@ def function_static_property_function(prop, details, config):
 
 def define_atomic_propositions(config):
     cw = CodeWriter()
-    for propos, details in config['AtomicPropositions'].items():
+    for propos, details in config['atomicPropositions'].items():
         v = Variable(propos, 'int', value=details['init'])
         cw.add_variable_initialization(v)
     return cw
 
 def function_compute_atomic_propositions(config):
     func = Function('compute_atomic_propositions', 'void')
-    for propos, details in config['AtomicPropositions'].items():
+    for propos, details in config['atomicPropositions'].items():
         # <TODO> parse spec statement from a DSL statement
         func.add_code(f'{propos} = {details["value"]};')
     return func
@@ -81,9 +97,9 @@ def function_policy():
 
 def function_initialization(config):
     func = Function('initialize', 'void')
-    for statevar, details in config['State'].items():
-        if details['type'] == 'Vector':
-            for dim in config['Dimensions']:
+    for statevar, details in config['state'].items():
+        if details['type'] == 'vector':
+            for dim in config['dimensions']:
                 func.add_code(f'{statevar}{dim} = __VERIFIER_nondet_int();')
             cond = []
             for box in details['ranges']:
@@ -96,7 +112,13 @@ def function_initialization(config):
                             cond_box.append(f'({statevar}{dim} >= {rnge[0]} && {statevar}{dim} <= {rnge[1]})')
                 cond.append('(' + " && ".join(cond_box) + ')')
             func.add_code(f'__VERIFIER_assume(({" || ".join(cond)}));')
-        if details['type'] == 'int':
+        elif details['type'] in config['actionTypes']:
+            func.add_code(f'{statevar} = __VERIFIER_nondet_int();')
+            cond = []
+            for act in config['actionTypes'][details['type']]['actions']:
+                cond.append(f'{statevar} == {act}')
+            func.add_code(f'__VERIFIER_assume(({" || ".join(cond)}));')
+        elif details['type'] == 'int':
             func.add_code(f'{statevar} = __VERIFIER_nondet_int();')
             cond = []
             for rnge in details['ranges']:
@@ -109,12 +131,8 @@ def function_initialization(config):
     return func
 
 def add_safety_specs_to_cw(cw, config):
-    for aspec in config['SafetySpecs']:
-        cw.add_line(f'if ({aspec}) {{')
-        cw.indent()
-        cw.add_line('__VERIFIER_error();')
-        cw.dedent()
-        cw.add_line('}')
+    for aspec in config['safetySpecs']:
+        cw.add_line(f'//@assert ({aspec});')
 
 def make_model_program(config):
     cw = CodeWriter()
@@ -138,13 +156,13 @@ def make_model_program(config):
             cw.add_variable_declaration(x)
     cw.add_line()
 
-    semantics = ['StateRobotPos']
-    action_semantic_funcs = [function_action_semantics_for_var(config, v) for v in semantics]
+    actions = ['StateRobotAct']
+    action_semantic_funcs = [function_action_semantics_for_act_var(config, v) for v in actions]
     for func in action_semantic_funcs:
         cw.add_function_definition(func)
         cw.add_line()
 
-    static_prop_funcs = [function_static_property_function(stprop, details, config) for stprop, details in config['StaticProperty'].items()]
+    static_prop_funcs = [function_static_property_function(stprop, details, config) for stprop, details in config['staticProperty'].items()]
     for func in static_prop_funcs:
         cw.add_function_definition(func)
     cw.add_line()
@@ -171,8 +189,8 @@ def make_model_program(config):
     main_cw.add_line('while (1)')
     main_cw.open_brace()
     main_cw.add_line(f'StateRobotAct = {policy_func.generate_call()};')
-    for func, var in zip(action_semantic_funcs, semantics):
-        main_cw.add_line(f'{func.name}({config["State"][var]["acted"]});')
+    for func in action_semantic_funcs:
+        main_cw.add_line(f'{func.generate_call()};')
     main_cw.add_function_call(compute_atprop_func)
     add_safety_specs_to_cw(main_cw, config)
     main_cw.close_brace()
@@ -186,7 +204,7 @@ def make_model_program(config):
 
 if __name__ == '__main__':
     import yaml
-    with open('../descriptions/1d-patrolling.yml', 'r') as stream:
+    with open('../descriptions/2d-patrolling.yml', 'r') as stream:
         config = yaml.safe_load(stream)
     cw = make_model_program(config)
     print(cw)
