b0116ee60eb2d35627a8b9db42eb4b77f12ee5b9
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-01-06 23:26:10 -0600

    Cleans ASP programs for DoorKey env

--- asp_minigrid.py
+++ asp_minigrid.py
@@ -11,45 +11,49 @@ def action_selection_policy_DoorKey_ground_truth(env: MiniGridEnv):
         return "up"
     if is_present(env, "key"):
         return "left"
+
     if is_present(env, "door") and check(env, env.front_pos, "door"):
         return " "
     if is_present(env, "door") and is_agent_facing(env, get_nearest(env, "door")) and not check(env, env.front_pos, "wall"):
         return "up"
     if is_present(env, "door"):
         return "left"
+
     if is_present(env, "goal") and is_agent_facing(env, get_nearest(env, "goal")):
         return "up"
     if is_present(env, "goal"):
         return "left"
+
     return "up"
 
-# another version of the ground truth where 'right' and 'left' actions are BOTH used
-def action_selection_policy_DoorKey_ground_truth_right(env: MiniGridEnv):
+
+# Alternate ground truth where 'right' and 'left' actions are used
+def action_selection_policy_DoorKey_ground_truth_directions(env: MiniGridEnv):
     if is_present(env, "key") and check(env, env.front_pos, "key"):
         return "pageup"
     if is_present(env, "key") and is_agent_facing(env, get_nearest(env, "key")):
         return "up"
-    if is_present(env, "key"):
-        if is_at_agents_right(env, get_nearest(env, "key")):
-            return 'right'
-        else:
-            return 'left'
+    if is_present(env, "key") and is_at_agents_right(env, get_nearest(env, "key")):
+        return 'right'
+    if is_present(env, "key") and is_at_agents_left(env, get_nearest(env, "key")):
+        return 'left'
+
     if is_present(env, "door") and check(env, env.front_pos, "door"):
         return " "
     if is_present(env, "door") and is_agent_facing(env, get_nearest(env, "door")) and not check(env, env.front_pos, "wall"):
         return "up"
-    if is_present(env, "door"):
-        if is_at_agents_right(env, get_nearest(env, "door")):
-            return 'right'
-        else:
-            return 'left'
+    if is_present(env, "door") and is_at_agents_right(env, get_nearest(env, "door")):
+        return 'right'
+    if is_present(env, "door") and is_at_agents_left(env, get_nearest(env, "door")):
+        return 'left'
+
     if is_present(env, "goal") and is_agent_facing(env, get_nearest(env, "goal")):
         return "up"
-    if is_present(env, "goal"):
-        if is_at_agents_right(env, get_nearest(env, "goal")):
-            return 'right'
-        else:
-            return 'left'
+    if is_present(env, "goal") and is_at_agents_right(env, get_nearest(env, "goal")):
+        return 'right'
+    if is_present(env, "goal") and is_at_agents_left(env, get_nearest(env, "goal")):
+        return 'left'
+
     return "up"
 
 
@@ -75,5 +79,5 @@ def action_selection_policy_decision_tree(env: MiniGridEnv, model, extract_featu
 
 
 ground_truth_asp_register = {
-    "MiniGrid-DoorKey-16x16-v0": action_selection_policy_DoorKey_ground_truth_right,
+    "MiniGrid-DoorKey-16x16-v0": action_selection_policy_DoorKey_ground_truth_directions,
 }
