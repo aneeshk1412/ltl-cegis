3f86775872d3d2f72e978a7edc5a772442b3f2fe
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-02-20 16:58:21 -0600

    Adds type hints to Verifier

--- verifier_minigrid.py
+++ verifier_minigrid.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-from copy import deepcopy
 import gymnasium as gym
+from copy import deepcopy
+from typing import Tuple, List, Callable
 
+from graph_utils import Trace
 from runner_minigrid import Runner
 from utils import load_list_from_pickle
-from graph_utils import Trace
 
 from minigrid.utils.window import Window
 from minigrid.minigrid_env import MiniGridEnv
 
 
 class Verify(Runner):
-    def run(self):
+    def run(self) -> Tuple[bool, List[Trace]]:
         super().run_internal()
         self.traces = [Trace(t, "cex") for t in self.traces if t]
         return (self.sat, self.traces)
@@ -26,7 +27,7 @@ class Verify(Runner):
 
 
 class VerifyAll(Runner):
-    def run(self):
+    def run(self) -> Tuple[bool, List[Tuple[bool, Trace]]]:
         super().run_internal()
         if not self.traces[-1]:
             self.traces = self.traces[:-1]
@@ -41,14 +42,14 @@ class VerifyAll(Runner):
 
 
 def verify_policy(
-    env_name,
-    policy,
-    seed=None,
-    num_rruns=20,
-    max_steps=100,
-    show_window=False,
-    block=False,
-    use_saved_envs=False,
+    env_name: str,
+    policy: Callable[[MiniGridEnv], str],
+    seed: int | None = None,
+    num_rruns: int = 20,
+    max_steps: int = 100,
+    show_window: bool = False,
+    block: bool = False,
+    use_saved_envs: bool = False,
 ):
     renv: MiniGridEnv = gym.make(env_name, tile_size=32, max_steps=max_steps)
     env_list = (
@@ -68,7 +69,6 @@ def verify_policy(
     )
     sat, traces = verifier.run()
     if show_window and len(traces) > 0:
-        window = Window(env_name)
         envs = [deepcopy(t[0][0]) for t in traces]
         cex_runs = VerifyAll(
             env_name=env_name,
@@ -76,12 +76,11 @@ def verify_policy(
             policy=policy,
             seed=seed,
             max_steps=max_steps,
-            window=window,
+            window=Window(env_name),
             block=block,
             env_list=envs,
         )
         _, _ = cex_runs.run()
-        window.close()
     return sat, traces
 
 
@@ -121,6 +120,7 @@ if __name__ == "__main__":
         env_name=env_name,
         policy=ground_truth_asp_register[env_name],
         use_saved_envs=False,
+        show_window=True
     )
     print(sat)
     print(len(traces))
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     print(sat)
     print()
     env = traces[0][len(traces[0]) // 2][0]
-    env_list = list(load_list_from_pickle(env_name + "-envs.pkl")) + [env]
+    env_list = [env]
 
     sat, sat_trace_pairs = verify_policy_on_envs(
         env_name=env_name,
