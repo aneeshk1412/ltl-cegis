6ebdc1db88ab3526dbeffa67ff8f703199a706a1
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-03-24 11:19:43 -0500

    Adds a learner skeleton

---
+++ learner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from custom_types import Policy, Features, Action
+
+
+def learn_policy(decisions: dict[Features, Action], task: str = "minigrid", args = None) -> Policy:
+    pass
