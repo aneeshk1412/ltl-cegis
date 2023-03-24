a73e93f3e865e8415ab7ce05cfe8f1248b6d8495
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-03-24 01:58:51 -0500

    Adds verifier skeleton

---
+++ verifier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+
+from trace import Trace
+from custom_types import Policy
+
+
+def verify_policy(policy: Policy, task: str = "minigrid") -> Tuple[bool, Trace]:
+    pass
