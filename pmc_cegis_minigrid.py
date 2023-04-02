cf4016dc9853c9d4f6d928a8395a53a84200049a
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-04-02 11:58:25 -0500

    Updates pmc algorithm to use arguments

--- pmc_cegis_minigrid.py
+++ pmc_cegis_minigrid.py
@@ -1,66 +1,52 @@
 #!/usr/bin/env python3
 
-import argparse
 import gymnasium as gym
 
 from learner_minigrid import learn
-from dsl_minigrid import features_empty
+from dsl_minigrid import feature_mapping
 from simulator_minigrid import simulate_policy_on_state
-from commons_minigrid import Decisions, pickle_to_demo_traces, PartialMDP
+from commons_minigrid import Decisions, pickle_to_demo_traces, PartialMDP, parse_args, debug
 
 from minigrid.minigrid_env import MiniGridEnv
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--env", help="gym environment to load", default="MiniGrid-MultiRoom-N6-v0"
-    )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        help="random seed to generate the environment with",
-        default=None,
-    )
-    parser.add_argument(
-        "--max-steps", type=int, help="number of steps to timeout after", default=100
-    )
-    parser.add_argument(
-        "--threshold", type=int, help="threshold to declare safe", default=200
-    )
-    args = parser.parse_args()
+    args = parse_args()
 
     partialMDP = PartialMDP()
 
-    demonstrations = pickle_to_demo_traces(args.env)
+    demonstrations = pickle_to_demo_traces(args.env_name)
     for demo in demonstrations:
         partialMDP.add_trace(demo)
 
     decisions = Decisions()
     for demo in demonstrations:
-        decisions.add_decision_list([(features_empty(s), a) for s, a, _ in demo])
+        decisions.add_decision_list([(feature_mapping[args.env_name](s), a) for s, a, _ in demo])
 
     policy, model = learn(decisions)
 
-    randomstate: MiniGridEnv = gym.make(args.env, tile_size=32)
+    randomstate: MiniGridEnv = gym.make(args.env_name, tile_size=32)
+    cegis_epochs = 0
     count = 0
     while True:
         randomstate.reset()
         sat, trace = simulate_policy_on_state(
             state=randomstate,
             policy=policy,
-            feature_fn=features_empty,
-            spec='P>=1 [F "is_agent_on__goal"]',
+            feature_fn=feature_mapping[args.env_name],
+            spec=args.spec,
+            args=args,
         )
         count += 1
+
         if count >= args.threshold:
             print("Found Satisfying Policy")
             break
         partialMDP.add_trace(trace)
         if not sat:
-            print(f"Counterexample found after : {count} runs")
+            print(f"CEGIS Epoch: {cegis_epochs}, Counterexample found after : {count} runs")
+            cegis_epochs += 1
             count = 0
-            # decisions = partialMDP.get_decisions(spec='P>=1 [F "is_agent_on__goal"]', feature_fn=features_empty)
+            # decisions = partialMDP.get_decisions(spec=args.spec, feature_fn=feature_mapping[args.env_name])
             policy, model = learn(decisions)
+            # Assert success on all known environments
