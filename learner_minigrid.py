2ea6cf663b0c415d46fede41c3255c12d13f729b
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-04-08 17:22:30 -0500

    Adds policy augmentation

--- learner_minigrid.py
+++ learner_minigrid.py
@@ -3,15 +3,25 @@
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
-from typing import Set, Tuple, Callable, List
+from typing import Set, Tuple, List
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
-from commons_minigrid import Decisions, debug, Features
+from commons_minigrid import Decisions, debug, Features, Policy, Action
 
 
-def policy_decision_tree(model: DecisionTreeClassifier):
-    def policy(feats: Features):
-        df = pd.DataFrame([feats.key])
+def augment_policy(policy: Policy, decisions: Decisions) -> Policy:
+    def new_policy(feats: Features) -> Action:
+        if feats in decisions.features_to_actions:
+            return list(decisions.features_to_actions[feats])[0]
+        else:
+            return policy(feats)
+
+    return new_policy
+
+
+def policy_decision_tree(model: DecisionTreeClassifier) -> Policy:
+    def policy(feats: Features) -> Action:
+        df = pd.DataFrame([feats.id])
         return model.predict(df)[0]
 
     return policy
@@ -21,7 +31,7 @@ def learn(
     decisions: Decisions,
     seed: int | None = None,
     save: bool = False,
-) -> Tuple[Callable[[Features], str], DecisionTreeClassifier]:
+) -> Tuple[Policy, DecisionTreeClassifier]:
     keys_list, actions_list = zip(*decisions.get_decisions())
     keys_df = pd.DataFrame(keys_list)
     actions_df = pd.DataFrame(actions_list)
